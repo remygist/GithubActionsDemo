fb0ed80cc0e16c989c5578d4cf56363529b7c5f5
remygist <remy.gistelinck@student.ehb.be>
2024-11-26 11:09:26 +0100

    made a custom action

---
+++ index.js
@@ -0,0 +1,8 @@
+const core = require('@actions/core');
+
+try {
+    const message = core.getInput('message');
+    console.log(`Message: ${message}`);
+} catch (error) {
+    core.setFailed(error.message);
+}
