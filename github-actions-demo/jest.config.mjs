c5b7f2030f89f994407dd22da5d0f82081916e94
remygist <remy.gistelinck@student.ehb.be>
2024-12-01 19:54:38 +0100

    wrote custom test

---
+++ github-actions-demo/jest.config.mjs
@@ -0,0 +1,12 @@
+export default {
+    rootDir: './',
+    testEnvironment: 'jest-environment-jsdom', 
+    transform: {
+      '^.+\\.(js|jsx|ts|tsx)$': 'babel-jest',
+    },
+    moduleNameMapper: {
+      '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+    },
+    setupFilesAfterEnv: ['@testing-library/jest-dom'],
+  };
+  
