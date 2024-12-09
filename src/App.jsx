9baacbc37376a44cbac7fba589f6bbe7be6a63a6
remygist <remy.gistelinck@student.ehb.be>
2024-12-09 02:16:05 +0100

    made changes for demo

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,7 @@ function App() {
           count is {count}
         </button>
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR MOM I DID IT LOOOk
+          Edit <code>src/App.jsx</code> and save to test
         </p>
       </div>
       <p className="read-the-docs">
